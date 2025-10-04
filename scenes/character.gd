65f24b44e4edda2577819c703f244cee6689f7d5
DazyLude <36658009+DazyLude@users.noreply.github.com>
2025-10-04 11:24:33 +0400

    basic scene layout + some reused code

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,2 +1,10 @@
 extends Node2D
 class_name Character
+
+
+signal encounter
+
+
+func _ready() -> void:
+	$Area2D.area_entered.connect(encounter.emit);
+	
--- scenes/item.gd
+++ scenes/item.gd
@@ -1,2 +1,42 @@
 extends Node2D
 class_name Item
+
+
+signal picked(event_pos)
+signal dropped(event_pos)
+
+
+var hovered : bool = false;
+var held : bool = false;
+
+
+func _ready() -> void:
+	$Area2D.mouse_entered.connect(_on_hover);
+	$Area2D.mouse_exited.connect(_on_stop_hover);
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_LEFT when event.pressed and hovered and not held:
+				_on_pick(event.position);
+				get_viewport().set_input_as_handled();
+			MOUSE_BUTTON_LEFT when not event.pressed and held:
+				_on_drop(event.position);
+				get_viewport().set_input_as_handled();
+
+
+func _on_hover() -> void:
+	hovered = true;
+
+
+func _on_stop_hover() -> void:
+	hovered = false;
+
+
+func _on_pick(mouse_pos: Vector2) -> void:
+	picked.emit(mouse_pos);
+
+
+func _on_drop(mouse_pos: Vector2) -> void:
+	dropped.emit(mouse_pos);
